ecfda4b3d2c8cb4b2c54f782dce9de983468e018
Koha <khoaendy@gmail.com>
2025-05-19 15:41:40 +0700

    Fix Update Province

--- FoodWebsite_API/DTOs/Province/ProvinceUpdateDTO.cs
+++ FoodWebsite_API/DTOs/Province/ProvinceUpdateDTO.cs
@@ -1,13 +1,19 @@
-﻿namespace FoodWebsite_API.DTOs.Province
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodWebsite_API.DTOs.Province
 {
     public class ProvinceUpdateDTO
     {
+        [Required]
         public string Region { get; set; }
         public string RegionPlain { get; set; }
+        [Required]
         public string Name { get; set; }
         public string NamePlain { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public int Version { get; set; }
         public bool IsActive { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
