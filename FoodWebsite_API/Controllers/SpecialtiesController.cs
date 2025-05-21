f212a5a764e7fc4d5a78b2903038d8b4e5368650
Hai3000 <hailove2k4@gmail.com>
2025-05-21 13:18:47 +0700

    suaphantimkiemchiloc1ketqua

--- FoodWebsite_API/Controllers/SpecialtiesController.cs
+++ FoodWebsite_API/Controllers/SpecialtiesController.cs
@@ -60,6 +60,20 @@ namespace FoodWebsite_API.Controllers
             return Ok(result);
         }
 
+        //search 1 kết quả
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Specialty>>> SearchSpecialty([FromQuery] string query)
+        {
+            if (string.IsNullOrEmpty(query))
+                return BadRequest("Thiếu từ khóa");
+
+            var result = await _context.Specialties
+                .Where(s => s.Name.ToLower().Contains(query.ToLower()))
+                .ToListAsync();
+
+            return Ok(result);
+        }
+
 
         //craeate
         [HttpPost]
