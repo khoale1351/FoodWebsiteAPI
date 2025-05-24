ab57cc1893eec9609057f574d5fbe9409d13092a
Koha <khoaendy@gmail.com>
2025-05-24 14:15:51 +0700

    Thêm ảnh cho các đặc sản

---
+++ FoodWebsite_API/DTOs/SpecialtyImages/SpecialtyImageMultiUploadDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodWebsite_API.DTOs.SpecialtyImages
+{
+    public class SpecialtyImageMultiUploadDTO
+    {
+        public int SpecialtyId { get; set; }
+        public List<IFormFile>? Files { get; set; }
+    }
+}
